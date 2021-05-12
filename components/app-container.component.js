e579ae3b5b629a18ec834bfc9c8f2d7097696121
Eds <steadyede@live.co.uk>
2021-05-12 17:12:58 +0100

    thoughts feed visible

--- components/app-container.component.js
+++ components/app-container.component.js
@@ -1,46 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 
-import CreateThought from "./create-thought.component";
 import InspireMe from "./inspire-me.component";
 import DisplayThoughts from "./display-thoughts.component";
 import Header from "./header.component"
-// import { gratitudeLevel } from "../functions/gratitude-level"
-import { progressBar } from "../functions/progress-bar"
-import { thoughtsLength } from "../functions/thoughts-length"
 
 const AppContainer = () => {
-  const [progressBarWidth, setProgressBarWidth] = useState(0)
-  const [gratitudeVolume, setGratitudeLevel] = useState(0)
-
-  const setProgressBar = async () => {
-      const { level, intMarker } = await thoughtsLength();
-
-      setProgressBarWidth(`${intMarker}%`)
-      setGratitudeLevel(`${level}`)
-  }
-  
-  const afterThoughtCreated = () => {
-    setProgressBar()
-  }
-
   return (
-      <View style={styles.container}>
-        <Header style={styles.header} />
-        {/* <CreateThought style={styles.createThought} afterThoughtCreated={afterThoughtCreated}/> */}
-        
-        <View style={styles.progressBar}>
-            { gratitudeLevel(gratitudeVolume) }
-            { progressBar(progressBarWidth) }
-        </View>
-        <InspireMe style={styles.inspireMe} />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      
+        <View style={styles.container}>
+          <Header style={styles.header} />
 
-        <View style={styles.thoughtList}> 
-            <ScrollView>
-              <DisplayThoughts />
-            </ScrollView>
-        </View>
+          <InspireMe style={styles.inspireMe} />
 
+          <View style={styles.thoughtList}> 
+              <ScrollView>
+                <DisplayThoughts />
+              </ScrollView>
+          </View>
+
+        </View>
       </View>
   )
 }
@@ -51,12 +31,6 @@ const styles = StyleSheet.create({
   },
   header: {
     flex: 1
-  },
-  createThought: {
-    flex: 1
-  },
-  progressBar: {
-    flex: 0.5
   },
   inspireMe: {
     flex: 1
