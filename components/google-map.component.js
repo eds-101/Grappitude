320e862f203e9148a1b6c801e1e997f2e7c2ec4a
Eds <steadyede@live.co.uk>
2021-05-13 16:58:07 +0100

    basic map

---
+++ components/google-map.component.js
@@ -0,0 +1,26 @@
+import React,  { Component } from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
+
+export default class GoogleMap extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
