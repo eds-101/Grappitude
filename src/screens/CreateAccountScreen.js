26da4aa7908850f7321809c5c16f5cad9338dc07
Eds <steadyede@live.co.uk>
2021-05-13 18:21:14 +0100

    basic forms

--- src/screens/CreateAccountScreen.js
+++ src/screens/CreateAccountScreen.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Button } from 'react-native';
-import { createAccount } from '../../api/authentication'
-import EmailForm from '../../forms/EmailForm';
+import Form from '../../forms/Form';
 
 const CreateAccount = ({ navigation }) => {
-  return (
-    <EmailForm
-      buttonText="Submit Details"
-      onSubmit={createAccount}
-      onAuthentication={() => navigation.navigate('Home')}
-    >
-      <Button
-        title="Already registered? Login instead"
-        onPress={() => navigation.navigate('Login')}
-      />
-    </EmailForm>
-  );
+  return <Form />;
 };
+
+// const CreateAccount = ({ navigation }) => {
+//   return (
+//     <EmailForm
+//       buttonText="Submit Details"
+//       onSubmit={createAccount}
+//       onAuthentication={() => navigation.navigate('Home')}
+//     >
+//       <Button
+//         title="Already registered? Login instead"
+//         onPress={() => navigation.navigate('Login')}
+//       />
+//     </EmailForm>
+//   );
+// };
 
 export default CreateAccount;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import { Button } from 'react-native';
-import { login } from '../../api/authentication';
-import EmailForm from '../../forms/EmailForm'
-// import { setToken } from '../api/token';
+import Form from '../../forms/Form';
 
 const LoginScreen = ({ navigation }) => {
-  return (
-    <EmailForm
-      buttonText="Log into the app"
-      onSubmit={login}
-      onAuthentication={() => navigation.navigate('Home')}
-    >
-      <Button
-        title="Not registered? Create an account"
-        onPress={() => navigation.navigate('Create Account')}
-      />
-    </EmailForm>
-  );
+  return <Form />;
 };
+
 
 export default LoginScreen;
